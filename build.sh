84d245d496af91cc7301dd35c6f87bb2085e3cd2
andre.vanderwesthuysen <andre.vanderwesthuysen@noaa.gov>
2020-05-13 00:27:53 +0000

    Changed order of env var checks

--- build.sh
+++ build.sh
@@ -16,20 +16,6 @@ if [ "${NEMSDIR}" == "" ]
     echo "ERROR - Your NEMSDIR environment variable is not set"
     exit 1
 fi
-if [ "${NETCDF_CONFIG}" == "" ]
-    then 
-    echo "ERROR - Your NETCDF_CONFIG environment variable for WW3 is not set"
-    exit 1
-else
-    echo "NETCDF_CONFIG set to ${NETCDF_CONFIG}"
-fi
-if [ "${METIS_PATH}" == "" ]
-    then 
-    echo "ERROR - Your METIS_PATH environment variable for WW3 is not set"
-    exit 1
-else
-    echo "METIS_PATH set to ${METIS_PATH}"
-fi
 
 # Load modules
 source ${ROOTDIR}/modulefiles/hera/ESMF_NUOPC
@@ -47,6 +33,20 @@ make -f GNUmakefile distclean_NWM COMPONENTS="NWM"
 make -f GNUmakefile distclean_NEMS COMPONENTS="ADCIRC WW3 NWM ATMESH"
 
 # Default configuration for WW3 (on Hera)
+if [ "${NETCDF_CONFIG}" == "" ]
+    then 
+    echo "ERROR - Your NETCDF_CONFIG environment variable for WW3 is not set"
+    exit 1
+else
+    echo "NETCDF_CONFIG set to ${NETCDF_CONFIG}"
+fi
+if [ "${METIS_PATH}" == "" ]
+    then 
+    echo "ERROR - Your METIS_PATH environment variable for WW3 is not set"
+    exit 1
+else
+    echo "METIS_PATH set to ${METIS_PATH}"
+fi
 echo 'F90 NOGRB NC4 DIST MPI PDLIB SCRIP PR3 UQ FLX0 SEED LD2 ST4 STAB0 NL1 BT1 DB1 MLIM TR0 BS0 XX0 WNX1 WNT1 CRX1 CRT1 O0 O1 O2 O3 O4 O5 O6 O7 O14 O15 IC0 IS0 REF0' > $ROOTDIR/WW3/model/esmf/switch
 echo "WW3 configured with these switch file options: $(<${ROOTDIR}/WW3/model/esmf/switch)"
 cp $ROOTDIR/WW3/model/bin/comp.Intel comp
