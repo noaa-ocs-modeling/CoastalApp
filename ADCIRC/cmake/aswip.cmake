541e9fb439a318475a0df532d56990dfe09dcfa4
Guoming Ling <gling@nd.edu>
2020-10-20 17:52:16 -0400

    fixed a bug in hstart.F

---
+++ ADCIRC/cmake/aswip.cmake
@@ -0,0 +1,27 @@
+
+IF(BUILD_ASWIP)
+
+
+    SET(ASWIP_SOURCES ${CMAKE_SOURCE_DIR}/src/sizes.F 
+                      ${CMAKE_SOURCE_DIR}/src/global.F 
+                      ${CMAKE_SOURCE_DIR}/src/global_3dvs.F 
+                      ${CMAKE_SOURCE_DIR}/src/boundaries.F 
+                      ${CMAKE_SOURCE_DIR}/src/hashtable.F 
+                      ${CMAKE_SOURCE_DIR}/src/nodalattr.F 
+                      ${CMAKE_SOURCE_DIR}/src/mesh.F 
+                      ${CMAKE_SOURCE_DIR}/src/wind.F 
+                      ${CMAKE_SOURCE_DIR}/src/owiwind.F 
+                      ${CMAKE_SOURCE_DIR}/src/owiwind_netcdf.F 
+                      ${CMAKE_SOURCE_DIR}/thirdparty/KDTREE2/kdtree2.F 
+                      ${CMAKE_SOURCE_DIR}/src/owi_ice.F
+                      ${CMAKE_SOURCE_DIR}/wind/vortex.F 
+                      ${CMAKE_SOURCE_DIR}/wind/aswip.F )
+
+    ADD_EXECUTABLE(aswip ${ASWIP_SOURCES})
+
+    addCompilerFlags(aswip ${ADDITIONAL_FLAGS_ASWIP})
+    addLibVersion(aswip)
+
+    INSTALL(TARGETS aswip RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+
+ENDIF(BUILD_ASWIP)
